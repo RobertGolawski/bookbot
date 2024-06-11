f8b1bb049278b91fa6675f5b4ce0cae856bb5f35
Robert Golawski <robert.golawski@yahoo.co.uk>
2024-06-11 11:20:25 +0100

    refactor

--- main.py
+++ main.py
@@ -2,34 +2,72 @@ def sort_by(dict):
     return dict["num"]
 
 
+def get_book(path):
+    with open(path) as f:
+        content = f.read()
+        return content
 
-
-
-
-with open("books/frankenstein.txt") as f:
-    content = f.read()
+def count_words(content):
     words = content.split()
+    return len(words)
 
-    all_chars = {}
+def count_letters(content):
+    dict = {}
+    for char in content:
+        char = char.lower()
+        if char in dict:
+            dict[char] += 1
+        else:
+            dict[char] = 1
+    return dict
 
-    for word in words:
-        for char in word:
-            char = char.lower()
-            if char in all_chars:
-                all_chars[char]+=1
-            else:
-                all_chars[char] = 1
+def list_dicts(dict):
+    l = []
+    for key in dict:
+        if key.isalpha():
+            l.append({"letter": key, "num": dict[key]})
     
-    list_of_dicts = []
+    l.sort(reverse=True, key=sort_by)
+    return l
 
-    for char in all_chars:
-        # print(f"{char} appears {all_chars[char]} times")
-        if char.isalpha():
-            dict = {"letter": char, "num": all_chars[char]}
-            list_of_dicts.append(dict)
 
-    list_of_dicts.sort(reverse=True, key=sort_by)
+def main(path):
+    book = get_book(path)
+    word_count = count_words(book)
+    letter_count = count_letters(book)
 
-    for dict in list_of_dicts:
+    list = list_dicts(letter_count)
+
+    for dict in list:
         print(f"The letter {dict['letter']} was found {dict['num']} times")
+
+
+main("books/frankenstein.txt")
+
+# with open("books/frankenstein.txt") as f:
+#     content = f.read()
+#     words = content.split()
+
+#     all_chars = {}
+
+#     for word in words:
+#         for char in word:
+#             char = char.lower()
+#             if char in all_chars:
+#                 all_chars[char]+=1
+#             else:
+#                 all_chars[char] = 1
+    
+#     list_of_dicts = []
+
+#     for char in all_chars:
+#         # print(f"{char} appears {all_chars[char]} times")
+#         if char.isalpha():
+#             dict = {"letter": char, "num": all_chars[char]}
+#             list_of_dicts.append(dict)
+
+#     list_of_dicts.sort(reverse=True, key=sort_by)
+
+#     for dict in list_of_dicts:
+#         print(f"The letter {dict['letter']} was found {dict['num']} times")
 
